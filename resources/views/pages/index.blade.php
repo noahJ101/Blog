bf8f75bce785238e649e2acb0df5448d721935b8
James <noahjamesobekpa101@gmail.com>
2024-02-18 15:15:18 -0800

    New

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{config('app.name', "BLOG")}}</title>
+</head>
+<body>
+    <h1>Welcome To Laravel</h1>
+    <p>This is the Laravel application from the Laravel from scratch YouTube Series</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PagesController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
+
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
